634acbaba6ed2b7b5d50d07e86aa1fd998088d9e
HAO Zhaojun <haozhaojun@eisti.eu>
2019-08-22 17:01:10 +0800

    Add files via upload

---
+++ conf_loader.py
@@ -0,0 +1,7 @@
+corpus_tfidf_mm = "corpus_tfidf.mm"
+corpora_dir = "crawl_news"  # 爬取的新闻都放在该文件夹下,每个新闻一个.txt文件
+corpora_path = "corpora.txt"
+id2word_path = "id2word"
+model_dir = "model"
+model_fname = "crawl_news.model"
+num_topics = 10
